36cef6046b71363038b97c5baf3125f89c440f37
fev <feventolla99@gmail.com>
2023-11-09 14:36:04 +0300

    resolve

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/layout/NavBar";
 import HomePage from "@/components/home/HomePage";
+import About from "@/components/About/about";
 
 export default function Home() {
   return (
     <main className="bg-white">
       <NavBar />
       <HomePage />
+      <About />
     </main>
   );
 }
