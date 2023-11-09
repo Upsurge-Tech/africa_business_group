56fbfeabfaeb178ab568edf10a9ca3e2286c4cdf
fev <feventolla99@gmail.com>
2023-11-09 09:40:58 +0300

    about_us

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,12 @@ import { Inter } from "next/font/google";
 import NavBar from "@/components/layout/NavBar";
 import HomePage from "@/components/home/HomePage";
 import Contact from "@/components/contact/contact";
+// import Image from "next/image";
+// import { Inter } from "next/font/google";
+
+import About from "@/components/About/about";
+
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
