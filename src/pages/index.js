3350f0d30449224bdf383bd372f0b1f339e45017
yonatan-h <yon019hl@gmail.com>
2023-11-09 19:09:15 +0300

    handle merge

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import NavBar from "@/components/layout/NavBar";
-import HomePage from "@/components/home/HomePage";
-import Contact from "@/components/contact/contact";
-import About from "@/components/About/about";
-import Partners from "@/components/partner/partner";
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import NavBar from '@/components/layout/NavBar'
+import HomePage from '@/components/home/HomePage'
+import Contact from '@/components/contact/contact'
+import About from '@/components/About/about'
+import Partners from '@/components/partner/partner'
 import OurMissionAndVision from '@/components/our-mission-and-vision'
+import Services from '@/components/services'
 
 export default function Home() {
   return (
@@ -15,7 +15,8 @@ export default function Home() {
       <HomePage />
       <About />
       <OurMissionAndVision />
-      <Partners/>
+      <Services />
+      <Partners />
       <Contact />
     </main>
   )
