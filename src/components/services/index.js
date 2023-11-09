22aa48651a54455a35cc4905bd8ef33b33eb5bc7
yonatan-h <yon019hl@gmail.com>
2023-11-09 18:53:28 +0300

    finish services desktop

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -23,7 +23,7 @@ export default function Services() {
   }
 
   const images1 = (
-    <div className="flex ">
+    <div className="flex-1 flex-grow flex ">
       {/* images */}
       <img
         className="flex-1 min-w-0 object-cover"
@@ -43,7 +43,7 @@ export default function Services() {
   )
 
   const images2 = (
-    <div className="flex">
+    <div className="flex-1 flex-grow flex">
       {/* images  */}
       <img
         className="flex-1 min-w-0 object-cover "
@@ -57,20 +57,21 @@ export default function Services() {
   )
 
   return (
-    <>
+    <section>
       <div className="flex flex-col items-center justify-center bg-black min-h-[128px]">
         {/* header */}
         <img src="/images/services/deco-line.svg" />
         <h2 className="text-white text-[35px]">Services</h2>
       </div>
 
-      <div className="p-[2rem]">
-        <div className=" text-[#4A5568] flex flex-col gap-[81px]">
+      <div className="px-[2rem] py-[90px]">
+        <div className=" text-[#4A5568] flex flex-col gap-[81px] max-w-[1244px] m-auto">
           <ServicesCard
             imageComponent={images1}
             title={text1.title}
             content={text1.content}
             onReadMore={text1.onReadMore}
+            flip={false}
           />
 
           <ServicesCard
@@ -78,10 +79,11 @@ export default function Services() {
             title={text2.title}
             content={text2.content}
             onReadMore={text2.onReadMore}
+            flip={true}
           />
         </div>
       </div>
-    </>
+    </section>
   )
 }
 function ReadMoreButton({ onClick }) {
@@ -94,12 +96,19 @@ function ReadMoreButton({ onClick }) {
   )
 }
 
-function ServicesCard({ imageComponent, title, content, onReadMore }) {
+function ServicesCard({ imageComponent, title, content, onReadMore, flip }) {
   return (
-    <div className="bg-[#F1F2FF] rounded-[30px] border">
-      {imageComponent}
+    <div
+      className={
+        'bg-[#F1F2FF] rounded-[30px] border flex flex-col ' +
+        (flip ? 'md:flex-row-reverse' : 'md:flex-row')
+      }
+    >
+      <div className="md:min-h-[418px] md:min-w-[418px] flex flex-col">
+        {imageComponent}
+      </div>
 
-      <div className="p-5 flex flex-col gap-[30px]">
+      <div className="flex-1 p-5 flex-col flex gap-[30px] md:justify-center md:p-[70px] ">
         <h3 className="text-[28px] text-[#2D3748] font-semibold">{title}</h3>
         <p>{content}</p>
         <ReadMoreButton onClick={onReadMore} />
