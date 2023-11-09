2cdc8cd02c6b7a9529d134aec05527ca1623fc2f
yonatan-h <yon019hl@gmail.com>
2023-11-09 19:00:37 +0300

    fix click on readmore

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -89,7 +89,10 @@ export default function Services() {
 function ReadMoreButton({ onClick }) {
   return (
     <div className="flex justify-end">
-      <button className="text-[#00407B] font-semibold text-[14px]">
+      <button
+        onClick={onClick}
+        className="text-[#00407B] font-semibold text-[14px]"
+      >
         Read more &gt;
       </button>
     </div>
