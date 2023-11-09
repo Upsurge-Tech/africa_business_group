ee8c2a5936147704810408e2c87d80427644bdfd
yonatan-h <yon019hl@gmail.com>
2023-11-09 16:42:34 +0300

    import images from figma

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -2,27 +2,50 @@ export default function Services() {
   return (
     <div>
       <div>
-        <h3>
-          Export and distribution of industrial and construction minerals.
-        </h3>
-        <p>
-          Our expertise in mineral export includes industrial and construction
-          minerals. We excel in quality control, reliable supply, and adherence
-          to international standards, ensuring you can trust us as your partner
-          in this sector.
-        </p>
-        <button>Read more &gt;</button>
+        {/* images */}
+        <div>
+          <img src="/images/services/coal.jpg" />
+          <div>
+            <img src="/images/services/red-salt.jpg" />
+            <img src="/images/services/white-rocks.jpg" />
+          </div>
+        </div>
+
+        <div>
+          {/* text */}
+
+          <h3>
+            Export and distribution of industrial and construction minerals.
+          </h3>
+          <p>
+            Our expertise in mineral export includes industrial and construction
+            minerals. We excel in quality control, reliable supply, and
+            adherence to international standards, ensuring you can trust us as
+            your partner in this sector.
+          </p>
+          <button>Read more &gt;</button>
+        </div>
       </div>
 
       <div>
-        <h3>Export of agricultural products</h3>
-        <p>
-          Our agricultural product export services cover a wide range of items,
-          including avocados, livestock, and perishable goods. We prioritize
-          freshness and international compliance, offering a comprehensive
-          solution for businesses looking to expand their agricultural exports.
-        </p>
-        <button>Read more &gt;</button>
+        <div>
+          {/* images  */}
+          <img src="/images/services/avocado.jpg" />
+          <img src="/images/services/cattle.jpg" />
+        </div>
+
+        <div>
+          {/* text */}
+          <h3>Export of agricultural products</h3>
+          <p>
+            Our agricultural product export services cover a wide range of
+            items, including avocados, livestock, and perishable goods. We
+            prioritize freshness and international compliance, offering a
+            comprehensive solution for businesses looking to expand their
+            agricultural exports.
+          </p>
+          <button>Read more &gt;</button>
+        </div>
       </div>
     </div>
   )
