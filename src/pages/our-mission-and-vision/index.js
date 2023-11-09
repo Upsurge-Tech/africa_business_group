764ad729e6189d99b79b0052defff80ed9508b1b
yonatan-h <yon019hl@gmail.com>
2023-11-09 14:35:48 +0300

    finished styling

--- src/pages/our-mission-and-vision/index.js
+++ src/pages/our-mission-and-vision/index.js
@@ -11,9 +11,10 @@ export default function OurMissionAndVision() {
   ]
 
   return (
-    <div className="p-[2rem]">
+    <div className="pt-[2rem] pb-[5rem] bg-[#262626]">
       <div className="flex flex-col gap-[44px] m-auto  max-w-[1211px]">
         <div>
+          <img src="/images/vision-and-mission/deco-line.svg" />
           <p className="text-[35px]">Know the Business</p>
           <h2 className="text-[35px] font-bold">Our Vision and Mission</h2>
         </div>
@@ -23,12 +24,14 @@ export default function OurMissionAndVision() {
             bulletIcon={'/images/vision-and-mission/target.svg'}
             title={'Mission'}
             statements={missions}
+            hasBlueCircle={true}
           />
 
           <ValueMissionCard
             bulletIcon={'/images/vision-and-mission/setting.svg'}
             title={'Vision'}
             statements={visions}
+            hasBlueCircle={false}
           />
         </div>
       </div>
@@ -36,44 +39,33 @@ export default function OurMissionAndVision() {
   )
 }
 
-function ValueMissionCard({ bulletIcon, title, statements }) {
-  const commonClass =
-    ' rounded-[100%] transform translate-x-[-50%] translate-y-[-50%] absolute blur-[121px]'
+function ValueMissionCard({ bulletIcon, title, statements, hasBlueCircle }) {
   return (
-    <div className="bg-[white] relative border overflow-hidden rounded-[23px] text-black p-[35px]">
-      {/* eclipses */}
-      <div
-        className={
-          'bg-[#ADADAD] w-[545px] h-[1047px] opacity-[67%] top-[-40%] left-[20%] ' +
-          commonClass
-        }
-      ></div>
-      <div
-        className={
-          'bg-[#D4D4D4] w-[343px] h-[658px] opacity-[58%] top-[40%] left-[90%] ' +
-          commonClass
-        }
-        div
-      ></div>
-      <div
-        className={
-          'bg-[#979797] w-[406px] h-[780px] opacity-[67%] top-[100%] left-[75%]' +
-          commonClass
-        }
-      ></div>
+    <div className="relative flex-1 flex-grow bg flex flex-col">
+      <div className="bg-[white] relative border rounded-[23px] text-black z-[2] flex-1 flex flex-col justify-between">
+        <NoOverflowRegion />
+        <ShiningRegion />
 
-      {/* end eclipses */}
-
-      <div className="gap-[22px] flex flex-col z-[99]">
-        {statements.map((statement) => (
-          <ValueMissionStatement
-            key={statement}
-            statement={statement}
-            bulletIcon={bulletIcon}
-          />
-        ))}
-        <h3 className="text-center text-[32px] font-bold">{title}</h3>
+        <div className="gap-y-[22px] flex flex-col p-[35px] relative">
+          {statements.map((statement) => (
+            <ValueMissionStatement
+              key={statement}
+              statement={statement}
+              bulletIcon={bulletIcon}
+            />
+          ))}
+        </div>
+        <h3 className="pb-[22px] text-center text-[32px] font-bold relative">
+          {title}
+        </h3>
       </div>
+      {hasBlueCircle ? (
+        <div
+          className={
+            'bg-gradient-to-b from-[#66F7A9] to-[#00407B] rounded-[100%] w-[72px] h-[72px] absolute left-[-35px] bottom-[-35px] blur-[4px]'
+          }
+        ></div>
+      ) : null}
     </div>
   )
 }
@@ -87,36 +79,70 @@ function ValueMissionStatement({ statement, bulletIcon }) {
   )
 }
 
+function NoOverflowRegion() {
+  const commonClass =
+    ' rounded-[100%] transform translate-x-[-50%] translate-y-[-50%] absolute blur-[121px] '
+  return (
+    <div className="absolute top-0 left-0 bottom-0 right-0 overflow-hidden rounded-[23px]">
+      {/* no overflow region */}
+      <div
+        className={
+          'bg-[#ADADAD] w-[545px] h-[1047px] opacity-[67%] top-[-40%] left-[20%] ' +
+          commonClass
+        }
+      ></div>
+
+      <div
+        className={
+          'bg-[#D4D4D4] w-[343px] h-[658px] opacity-[58%] top-[40%] left-[90%] ' +
+          commonClass
+        }
+      ></div>
+
+      <div
+        className={
+          'bg-[#979797] w-[406px] h-[780px] opacity-[67%] top-[100%] left-[75%]' +
+          commonClass
+        }
+      ></div>
+    </div>
+  )
+}
+
 function ShiningRegion() {
   const commonClass =
     ' rounded-[100%] transform translate-x-[-50%] translate-y-[-50%] absolute blur-[121px]'
   return (
-    <div className="h-[500px] bg-[white] relative border overflow-hidden blur-[74px] ">
-      <div
-        className={
-          'bg-[#CECECE] w-[453px] h-[869px] opacity-[67%] top-[-10%] left-[20%]' +
-          commonClass
-        }
-      ></div>
-      <div
-        className={
-          ' bg-[#D4D4D4] w-[284px] h-[547px] top-[33%] left-[85%]' + commonClass
-        }
-      ></div>
+    <div className="absolute translate-x-[-50%] translate-y-[-50%] left-[50%] top-[75%] w-[80%]">
+      <div className="w-[100%] h-[400px] bg-[white] relative border overflow-hidden blur-[74px]">
+        <div
+          className={
+            'bg-[#CECECE] w-[453px] h-[869px] opacity-[67%] top-[-10%] left-[20%]' +
+            commonClass
+          }
+        ></div>
+        <div
+          className={
+            ' bg-[#D4D4D4] w-[284px] h-[547px] top-[33%] left-[85%]' +
+            commonClass
+          }
+        ></div>
 
-      <div
-        className={
-          'bg-[#979797] w-[337px] h-[647px] opacity-[67%] top-[100%] left-[75%]' +
-          commonClass
-        }
-      ></div>
+        <div
+          className={
+            'bg-[#979797] w-[337px] h-[647px] opacity-[67%] top-[100%] left-[75%]' +
+            commonClass
+          }
+        ></div>
 
-      <div
-        className={
-          'bg-[#ADF475] w-[337px] h-[647px] top-[100%] left-[10%]' + commonClass
-        }
-      ></div>
-      {/* shining region */}
+        <div
+          className={
+            'bg-[#ADF475] w-[337px] h-[647px] top-[100%] left-[10%]' +
+            commonClass
+          }
+        ></div>
+        {/* shining region */}
+      </div>
     </div>
   )
 }
