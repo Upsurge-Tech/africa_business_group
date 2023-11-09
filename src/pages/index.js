8c4d57fe60be666e548b65f761e6b2c6ee431b13
fev <feventolla99@gmail.com>
2023-11-09 12:13:06 +0300

    contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/layout/NavBar";
 import HomePage from "@/components/home/HomePage";
+import Contact from "@/components/contact/contact";
 
 export default function Home() {
   return (
     <main className="bg-white">
       <NavBar />
       <HomePage />
+      <Contact />
     </main>
   );
 }
