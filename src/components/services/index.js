32485bb3d306207b869e0d8aa7fd8a20676467a5
yonatan-h <yon019hl@gmail.com>
2023-11-09 16:32:41 +0300

    copy text from figma

--- src/components/services/index.js
+++ src/components/services/index.js
@@ -1,7 +1,29 @@
 export default function Services() {
   return (
     <div>
-      <h1>services</h1>
+      <div>
+        <h3>
+          Export and distribution of industrial and construction minerals.
+        </h3>
+        <p>
+          Our expertise in mineral export includes industrial and construction
+          minerals. We excel in quality control, reliable supply, and adherence
+          to international standards, ensuring you can trust us as your partner
+          in this sector.
+        </p>
+        <button>Read more &gt;</button>
+      </div>
+
+      <div>
+        <h3>Export of agricultural products</h3>
+        <p>
+          Our agricultural product export services cover a wide range of items,
+          including avocados, livestock, and perishable goods. We prioritize
+          freshness and international compliance, offering a comprehensive
+          solution for businesses looking to expand their agricultural exports.
+        </p>
+        <button>Read more &gt;</button>
+      </div>
     </div>
   )
 }
